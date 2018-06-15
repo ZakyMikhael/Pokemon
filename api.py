bb42a0c0c4b0bd58cac3c4cb0ffc0d7aa22af170
zaky <zakymmikhael@gmail.com>
2018-06-15 14:10:29 +0200

    avec hug

--- api.py
+++ api.py
@@ -3,26 +3,41 @@
 import mysql.connector
 import hug
 
+conn = mysql.connector.connect(host="localhost", user="root", password="", database="pokemon")
+cursor = conn.cursor(buffered=True)
 
 
-@hug.get('/') # <-- This is the only additional line
-def all():
-    conn = mysql.connector.connect(host="localhost", user="root", password="", database="pokemon")
-    cursor = conn.cursor(buffered=True)
+@hug.get('/')
+def allpoke():
     cursor.execute("""SELECT * FROM pokedex""")
     result = cursor.fetchall()
-    conn.close()
     return result
 
-@hug.post('/')
-def post():
-    cursor.execute("""SELECT * FROM pokedex""")
-    result = cursor.fetchall()
-    conn.close()
-    return result
+@hug.get('/')
+def findbyid(tmp):
+        cursor.execute("""SELECT * FROM pokedex WHERE id =%s """, [tmp])
+        result = cursor.fetchall()
+        return result
+        return tmp
+        conn.commit()
 
 @hug.put('/')
-def my_func(name):
-    name += ""
-    return name
+def my_func(id):
+    id += "55"
+    return id
 
+
+@hug.delete('/')
+def deletename(tmp):
+
+    cursor.execute("""DELETE FROM pokedex WHERE id =%s""", [tmp])
+    conn.commit()
+    print(tmp)
+    return tmp
+
+
+@hug.post('/')
+def postid(id):
+    cursor.execute ("""INSERT INTO pokedex (id) VALUES ("") """)
+    conn.commit()
+    conn.close()
